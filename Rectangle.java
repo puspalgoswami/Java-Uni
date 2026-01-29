59fa3177bb200dcbfd2c36099acb51c2484ee837
Puspal Goswami <goswamipuspal2005@gmail.com>
2026-01-29 14:52:29 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,7 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        float length = (float) 7.6;
+        float breadth = (float) 3.4;
+        System.out.println(length*breadth);
+	}
+}
