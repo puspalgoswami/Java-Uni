3b467301dcdbc62caa133c1df718c2b9ae6b0a03
Puspal Goswami <goswamipuspal2005@gmail.com>
2026-03-26 14:53:32 +0530

    Update display messages in PointCircle.java

--- PointCircle.java
+++ PointCircle.java
@@ -8,7 +8,7 @@ class Point {
     }
 
     void displayPoint() {
-        System.out.println("Center Point: (" + x + "," + y + ")");
+        System.out.println("Center Point is (" + x + "," + y + ")");
     }
 }
 
@@ -23,16 +23,15 @@ class Circle {
 
     void displayCircle() {
         center.displayPoint();
-        System.out.println("Radius: " + radius);
+        System.out.println("Radius = " + radius);
+        System.out.println("Area = " + 2*(22/7)*radius);
     }
 }
 
-class PointCircle {
+class Main {
     public static void main(String[] args) {
         Point p = new Point(3, 4);
-
         Circle c = new Circle(p, 5.5);
-
         c.displayCircle();
     }
 }
